3f433bad15209d231ddac5c38b990460630657a0
mycreation278 <mycreation278@gmail.com>
2026-06-12 18:46:02 +0530

    Fix mobile viewport height and responsive layout issues

--- public/js/main.js
+++ public/js/main.js
@@ -274,5 +274,11 @@
       }
     });
   });
+  function setVh() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+  }
 
+  setVh();
+  window.addEventListener('resize', setVh, { passive: true });
+  window.addEventListener('orientationchange', setVh, { passive: true });
 })();
